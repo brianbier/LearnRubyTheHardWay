d98c43536de4eb2c3036728ee5fda011ee841554
Brian <briansbier@gmail.com>
2015-03-31 23:21:44 -0400

    add exercise 4

---
+++ ex4.rb
@@ -0,0 +1,17 @@
+#These are variables that are use to substitute specific items. For example the cars = 100 and at the second portion where the code is written its just substituting those words with the numbers. It makes it much easier rather than remembering stuff. Developers are lazy! 
+# The double == tests if two things have the same value and the single = only assigns a value from the right to the left.
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
