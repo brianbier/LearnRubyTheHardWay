35f4ed01bcadf32c5494be38c2fc155f198bc880
Brian <briansbier@gmail.com>
2015-03-31 23:19:14 -0400

    Add exercies 3

---
+++ ex3.rb
@@ -0,0 +1,30 @@
+# This line below will print the writing into terminal
+puts "I will now count my chickens:"
+# This line will count how many hens and roosters there is by just adding the total number. As you can see if you put the # sign it hides the actual calculation.
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+#This will ask the question and print it but then it will add the calculation and add place the correct number
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#This is asking a question without a calculation to do the calculations are done {} between those squgaly lines
+puts "Is it true that 3.4 + 2 < 5.5 - 7?"
+#This prints the answer to the question above.
+puts 3 + 2 < 5 - 7
+# This will just do calculations and give the answer. The # sign works as a way to hide he calculation but it will still show the answer because its inside the string.
+puts "What is 3.4 + 2? #{3.4 + 2}"
+puts "What is 5.5 - 7? #{5.5 - 7}"
+puts "Oh, that's why its false."
+puts "How about some more."
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+# This exercise is design to show you the math symbols and what they all mean
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
