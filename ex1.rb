14b35a8e981b67a8f882394607a55b33a2ae0db5
Brian <briansbier@gmail.com>
2015-03-31 22:51:21 -0400

    LRTHW Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+# puts "Yay! pounds rule"
+# The # sign actually is used for comments in the code to help you. Ruby will not use this as a code
