bb4f4304a3ba25af3f0815bea505cd5890728113
Brian <briansbier@gmail.com>
2015-04-05 19:14:07 -0400

    Added Exercises

--- ex15.rb
+++ ex15.rb
@@ -18,9 +18,7 @@ file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
 
-
-
 print txt_again.read
-txt_again = close(file_again)
+
 #What we want to do is "open" that file in our script and print it out. However, we do not want to just "hard code" the name ex15_sample.txt into our script. "Hard coding" means putting some bit of information that should come from the user as a string directly in our source code. 
 #That's bad because we want it to load other files later. The solution is to use ARGV or gets.chomp to ask the user what file to open instead of "hard coding" the file's name.
